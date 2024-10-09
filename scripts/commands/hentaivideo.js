b2baec79f045f69d806986dd35577d0e66ddf955
Tanvirahmed1234567 <dipto123132@gmail.com>
2024-10-09 13:10:40 +0600

    Update hentaivideo.js

--- scripts/commands/hentaivideo.js
+++ scripts/commands/hentaivideo.js
@@ -31,7 +31,7 @@ module.exports.run = async ({ api, event, args }) => {
         response.data.pipe(writer);
 
         writer.on('finish', () => {
-            api.sendMessage({ body: "♻️--『🅰🅻🆅🅸 🅱🅾🆃』--♻️", attachment: fs.createReadStream(videoPath) }, event.threadID);
+            api.sendMessage({ body: "♻️--『-𝗧𝗔𝗡𝗩𝗜𝗥-🅱🅾🆃』--♻️", attachment: fs.createReadStream(videoPath) }, event.threadID);
         });
 
         writer.on('error', (err) => {
